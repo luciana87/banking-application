7e6c93f17997c80aae0a55b9400f2922dc490501
luciana87 <ch.luciana87@gmail.com>
2023-07-28 14:24:11 -0300

    Actualización de estructura de clases y herencia, endpoint create de la clase Bank

--- src/main/java/com/bankapp/bankingapp/entity/Employee.java
+++ src/main/java/com/bankapp/bankingapp/entity/Employee.java
@@ -1,10 +1,16 @@
 package com.bankapp.bankingapp.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Employee extends Person {
     private int employeeNumber;
+    @OneToMany(mappedBy = "employee", fetch = FetchType.LAZY)
+    private List<Account> accounts;
 
     public Employee(){
 
@@ -13,9 +19,15 @@ public class Employee extends Person {
         this.employeeNumber = employeeNumber;
     }
 
-    public Employee(String name, String lastName, String cardNumber, long phoneNumber, String email, Address address, int employeeNumber) {
+    public Employee(int employeeNumber, List<Account> accounts) {
+        this.employeeNumber = employeeNumber;
+        this.accounts = accounts;
+    }
+
+    public Employee(String name, String lastName, String cardNumber, long phoneNumber, String email, Address address, int employeeNumber, List<Account> accounts) {
         super(name, lastName, cardNumber, phoneNumber, email, address);
         this.employeeNumber = employeeNumber;
+        this.accounts = accounts;
     }
 
     public int getEmployeeNumber() {
